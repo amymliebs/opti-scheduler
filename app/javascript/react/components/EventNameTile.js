bfc8566b66d4b1dc696bb79af4936e1908f13e53
amymliebs <amymliebs@gmail.com>
2019-10-26 15:27:57 -0400

    seed file updated with users and events

--- app/javascript/react/components/EventNameTile.js
+++ app/javascript/react/components/EventNameTile.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom"
 
 const EventNameTile = (props) => {
   return(
-    <Link to={`/events/${props.id}`}>
-      <div className="event-name">{props.eventName}</div>
-    <div className="rsvp-status">{props.rsvpStatus}</div>
-    </Link>
+    <>
+      <Link to={`/events/${props.id}`}>
+        <div className="event-name">{props.eventName}</div>
+      </Link>
+      <div className="rsvp-status">{props.rsvpStatus}</div>
+    </>
   )
 }
 
