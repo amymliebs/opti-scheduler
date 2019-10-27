e61ebd0f101211eec463178ef7471902826f815c
amymliebs <amymliebs@gmail.com>
2019-10-27 16:57:08 -0400

    new event form routes set up

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,4 +2,24 @@ class Api::V1::EventsController < ApiController
   def index
     render json: Event.all
   end
+
+  def create
+    event = Event.new(event_params)
+    event.user = current_user
+
+    if event.save
+      render json: event
+    else
+      render json: {
+        errors: event.errors.messages,
+        fields: event
+      }
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:event_name, :event_description, :event_date, :rsvp_date)
+  end
 end
---
+++ app/javascript/react/components/NewEventForm.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+
+const NewEventForm = (props) => {
+  return(
+    <div>New Event Form </div>
+  )
+}
+
+export default NewEventForm
