220635bdc0aba6c75f13c48f93ea7c9fac91de19
amymliebs <amymliebs@gmail.com>
2019-10-29 20:28:37 -0400

    action mailer config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,13 +65,14 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  
+
   host = "http://optischeduler.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
 
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
+   :domain               => "http://optischeduler.herokuapp.com",
    :user_name            => ENV['GMAIL_USERNAME'],
    :password             => ENV['GMAIL_PASSWORD'],
    :authentication       => "plain",
@@ -80,7 +81,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
