84e0bff0794da980b9950b9f4d52cf80cd76004c
amymliebs <amymliebs@gmail.com>
2019-10-30 20:36:25 -0400

    changed email addressees

--- app/mailers/correspondence_mailer.rb
+++ app/mailers/correspondence_mailer.rb
@@ -3,11 +3,10 @@ class CorrespondenceMailer < ApplicationMailer
     @event = event
     @user = event.user
     @invitee = invitee
-    @url = "www.optischeduler.herokuapp.com/events/#{event.id}/invitees/
-    #{invitee.id}"
+    @url = "www.optischeduler.herokuapp.com/events/#{event.id}/invitees/#{invitee.id}"
 
     mail(
-      bcc: @event.invitees,
+      bcc: @invitee.email,
       subject: "#{@user.first_name} #{@user.last_name} Has Invited You to an Event: #{@event.event_name}! Please RSVP"
     )
   end
